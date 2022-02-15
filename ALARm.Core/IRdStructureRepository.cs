095c13c1131ed972e74931694ff2e368bd0b68c3
Aniyar <45550980+Aniyar@users.noreply.github.com>
2022-02-15 09:30:20 +0600

    Update IRdStructureRepository.cs

--- ALARm.Core/IRdStructureRepository.cs
+++ ALARm.Core/IRdStructureRepository.cs
@@ -167,7 +167,6 @@ namespace ALARm.Core
         //List<Digression> TotalSleeper(long processId);
         public void UpdateGap(Gap gap);
         public int UpdateGapBase(Gap gap, Kilometer kilometer, RdAction action);
-        public int UpdateBoltBase(Digression bolt, Kilometer kilometer, RdAction action);
         public int InsertCorrection(long trip_id, int track_id, int Number, int coord, int CorrectionValue);
         public List<CorrectionNote> GetCorrectionNotes(long trip_id, int track_id, int Number, int coord, int CorrectionValue);
         public int UpdateDigressionBase(Digression digression, int type, Kilometer kilometer, RdAction action);
